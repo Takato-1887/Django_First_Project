710927592338e19575e35e8353fbef6ffac561f9
Takato-1887 <priyalbhatt.510@gmail.com>
2025-01-26 21:22:05 +0530

    Successfully implemented word counter

--- myproject/myapp/views.py
+++ myproject/myapp/views.py
@@ -4,4 +4,9 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     name = "Priyal"
-    return render(request, "index.html", {"name": name})
+    return render(request, "index.html")
+
+def counter(request):
+    text = request.GET["text"]
+    count = len(text.split())
+    return render(request, "counter.html",{"count": count})
