7c861abdf6d7e5d2bce3dd9b0d7f47cfdcb807a8
Takato-1887 <priyalbhatt.510@gmail.com>
2025-02-04 23:16:29 +0530

    Trying out Django models

--- myproject/myapp/views.py
+++ myproject/myapp/views.py
@@ -1,10 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from .models import Feature
 # Create your views here.
 def index(request):
     name = "Priyal"
-    return render(request, "index.html")
+    feature1 = Feature()
+    feature1.id = 0
+    feature1.name = "Hello Guys"
+    feature1.details="This is my django project"
+
+
+    return render(request, "index.html", {"feature": feature1})
 
 def counter(request):
     text = request.POST["text"]
