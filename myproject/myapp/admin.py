5dc50dc4de9ed1a19ec7354032f86fb6dfe4d14f
Takato-1887 <priyalbhatt.510@gmail.com>
2025-03-03 20:49:14 +0530

    Successfully implemented additional features like Filter and Search using Django Admin Dashboard

--- myproject/myapp/admin.py
+++ myproject/myapp/admin.py
@@ -1,5 +1,22 @@
 from django.contrib import admin
 from .models import Feature
 
-# Register your models here.
-admin.site.register(Feature)
+class FeatureAdmin(admin.ModelAdmin):
+    # Fields to display in the list view
+    list_display = ('name', 'details')
+
+    # Add a search bar
+    search_fields = ('name', 'details')
+
+    # Add filters
+    list_filter = ('name',)
+
+    # Enable editing directly from the list view
+    list_editable = ('details',)
+
+    # Set the number of items per page
+    list_per_page = 10
+
+
+# Register the model with the custom admin class
+admin.site.register(Feature, FeatureAdmin)
